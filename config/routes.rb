8bd35274b44276b1dd7040f5383d498576305e1d
kaalm1 <kaalm1@gmail.com>
2017-07-10 18:23:46 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+resources :users
+
+get '/', to: "sessions#index", as: "root"
+get '/login', to: "sessions#new", as: "login"
+post '/sessions', to: "sessions#create"
+delete '/logout', to: "sessions#destroy", as: "logout"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
